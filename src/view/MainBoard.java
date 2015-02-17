1957a133e5b3598e1e10678e39821587fb9f0473
Jiaqi Chen <jackychen901010@gmail.com>
2015-02-16 18:38:12 -0600

    modification

--- src/view/MainBoard.java
+++ src/view/MainBoard.java
@@ -2,11 +2,16 @@ package view;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.net.URL;
+
 import javax.swing.*;
 import javax.swing.text.*;
+
 import controller.Control;
 
 public class MainBoard extends JFrame{
+	final public ImageIcon iOpen = 
+		    new ImageIcon(getClass().getResource("/image/open.gif"));
 	private JTextArea area = new JTextArea(20,120);
 	private JFileChooser dialog = new JFileChooser(System.getProperty("user"));
 	private String currentFile = "Untitled";
@@ -19,6 +24,71 @@ public class MainBoard extends JFrame{
 			SaveAs.setEnabled(true);
 		}
 	};
+	private WindowListener w1 = new WindowListener(){
+		public void windowClosing(WindowEvent e)
+	    {
+			if(changed){
+	        JFrame frame = (JFrame)e.getSource();
+	 
+	        int result = JOptionPane.showConfirmDialog(
+	            frame,
+	            "Are you sure you want to exit the application without saving?",
+	            "Exit Application",
+	            JOptionPane.YES_NO_OPTION);
+	 
+	        if (result == JOptionPane.YES_OPTION)
+	            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        if(result == JOptionPane.NO_OPTION){
+	        	if(dialog.showSaveDialog(null)==JFileChooser.APPROVE_OPTION){
+	    			controller.saveFileAs(area,dialog);
+	    			currentFile = dialog.getSelectedFile().getAbsolutePath();
+	    			setTitle(currentFile);
+	    			changed = false;
+	    			Save.setEnabled(false);
+	    			}
+	        }
+			}
+	    }
+
+		@Override
+		public void windowOpened(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowClosed(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowIconified(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowActivated(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+	
+	
 	
 	public MainBoard(){
 		area.setFont(new Font("Monospaced",Font.PLAIN,12));
@@ -48,9 +118,9 @@ public class MainBoard extends JFrame{
 		
 		JButton cut = tool.add(Cut), cop = tool.add(Copy),pas = tool.add(Paste);
 		
-		cut.setText(null); cut.setIcon(new ImageIcon(System.getProperty("user.dir")+"/image/cut.gif"));
-		cop.setText(null); cop.setIcon(new ImageIcon(System.getProperty("user.dir")+"/image/copy.gif"));
-		pas.setText(null); pas.setIcon(new ImageIcon(System.getProperty("user.dir")+"/image/paste.gif"));
+		cut.setText(null); cut.setIcon(new ImageIcon(getClass().getResource("/image/cut.gif")));
+		cop.setText(null); cop.setIcon(new ImageIcon(getClass().getResource("/image/copy.gif")));
+		pas.setText(null); pas.setIcon(new ImageIcon(getClass().getResource("/image/paste.gif")));
 		
 		Save.setEnabled(false);
 		SaveAs.setEnabled(false);
@@ -58,11 +128,12 @@ public class MainBoard extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pack();
 		area.addKeyListener(k1);
+		this.addWindowListener(w1);
 		setTitle(currentFile);
 		setVisible(true);
 	}
 	
-	Action Open = new AbstractAction("Open", new ImageIcon("/Users/jiaqichen/Documents/workspace/TextEditor/image/open.gif")) {
+	Action Open = new AbstractAction("Open", iOpen) {
 		public void actionPerformed(ActionEvent e) {
 			if(changed){
 			controller.saveOld(area, dialog.getSelectedFile().getAbsolutePath());
@@ -81,7 +152,7 @@ public class MainBoard extends JFrame{
 		}
 	};
 	
-	Action Save = new AbstractAction("Save", new ImageIcon("/Users/jiaqichen/Documents/workspace/TextEditor/image/save.gif")) {
+	Action Save = new AbstractAction("Save", new ImageIcon(getClass().getResource("/image/save.gif"))) {
 		public void actionPerformed(ActionEvent e) {
 			if(!currentFile.equals("Untitled")){
 				controller.saveFile(area,currentFile);
@@ -123,7 +194,17 @@ public class MainBoard extends JFrame{
 	Action Cut = m.get(DefaultEditorKit.cutAction);
 	Action Copy = m.get(DefaultEditorKit.copyAction);
 	Action Paste = m.get(DefaultEditorKit.pasteAction);
+
+
+
+
+	private Icon ImageIcon(URL resource) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
+
+
 //	private void saveFileAs() {
 //		if(dialog.showSaveDialog(null)==JFileChooser.APPROVE_OPTION){
 //			controller.saveFile(area,dialog.getSelectedFile().getAbsolutePath());
